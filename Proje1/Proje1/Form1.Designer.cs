841c86324759c67d6068f49118161b4ac9132223
Yemre-karakuzu <yunus11414@gmail.com>
2020-07-20 23:15:42 +0300

    Tamamlandı

---
+++ Proje1/Proje1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Proje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 47);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(450, 250);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(468, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 600);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tarih:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Saat:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(792, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(316, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(265, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Toplam:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 659);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Proje1/Proje1/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using System.IO;
+using net.zemberek.erisim;
+using net.zemberek.tr.yapi;
+using System.Text.RegularExpressions;
+using Microsoft;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Proje1
+{
+    public partial class Form1 : Form
+    {
+        public Tesseract.Rect bounds; //segmentlerin görsel olarak gösterimi için çizilecek olan dikdörtgenin bilgileri
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string GetNumbers(string input)//gönderilen satırdaki sayilari geri döndürür
+        {
+            return new string(input.Where(c => char.IsDigit(c)).ToArray());
+        }
+        public float DigitOnly(string line)//toplam, tam sayı olsa bile virgül ile ifade edildiği için toplam satırındaki float değerini elde etme fonksiyonu
+        {
+            string sumLine = "";
+            float toplam = -1;
+            try
+            {
+                if (line.Contains(","))
+                {
+                    string[] x = line.Split(',');
+                    sumLine += GetNumbers(x[0]);
+                    sumLine += ".";
+                    sumLine += GetNumbers(x[1]);
+                    toplam = float.Parse(sumLine);
+                }
+                else
+                {
+                    string[] x = line.Split('.');
+                    sumLine += GetNumbers(x[0]);
+                    sumLine += ".";
+                    sumLine += GetNumbers(x[1]);
+                    toplam = float.Parse(sumLine);
+                }
+            }
+            catch
+            {
+
+            }
+            
+            return toplam;
+        }
+        public static Bitmap MakeGrayscale3(Bitmap original) // görseli grayscale hale dönüştürme fonksiyonu
+        {           
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+           
+            using (Graphics g = Graphics.FromImage(newBitmap))
+            {                
+                ColorMatrix colorMatrix = new ColorMatrix(
+                   new float[][]
+                   {
+             new float[] {.3f, .3f, .3f, 0, 0},
+             new float[] {.59f, .59f, .59f, 0, 0},
+             new float[] {.11f, .11f, .11f, 0, 0},
+             new float[] {0, 0, 0, 1, 0},
+             new float[] {0, 0, 0, 0, 1}
+                   });
+               
+                using (ImageAttributes attributes = new ImageAttributes())
+                {
+                   
+                    attributes.SetColorMatrix(colorMatrix);
+                  
+                    g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+                                0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+                }
+            }
+            return newBitmap;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {          
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) //dosya yaani görselin seçilip işleneceği button fonksiyonu
+        {
+            //tekrar kullanım için temizleme 
+            listView1.Items.Clear(); 
+            listView1.BackColor = Color.White;
+            string path = "";
+
+            //dosyanın seçim ekranı
+            OpenFileDialog dosya = new OpenFileDialog();
+
+            if (dosya.ShowDialog() == DialogResult.OK)
+            {
+                path = dosya.FileName;
+            }
+
+            var img = new Bitmap(path);
+            img = MakeGrayscale3(img);
+            var ocr = new TesseractEngine("./tessdata", "eng"); // ocr ın tanımlanması
+
+            pictureBox1.Image = img;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            var output = ocr.Process(img); //görselin string haline dönüştürülmesi
+            string text = output.GetText();
+
+            File.WriteAllText("output.txt", text); //elde edilen stringin txt e yazılması
+
+
+            var zemberek = new Zemberek(new TurkiyeTurkcesi()); //Nlp
+
+            pictureBox1.Image = img;
+
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("Urun", 200);
+            listView1.Columns.Add("Kdv", 100);
+            listView1.Columns.Add("Fiyat", 80);
+
+            string tarih;
+            string saat;          
+            float toplam = 0;
+            float toplamKontrol = 0;           
+            bool t = true;
+            string[] line = File.ReadLines("output.txt").ToArray();
+
+            //regex 
+            string pattern = @"(.+)+(%[0-9] +)+(.+)";           
+            string datePattern = @"(.*)([0-9][0-9])[\/]?.([0-9][0-9])[\/]?.(\d{4})(.*)";
+            string timePattern = @"([0-2]?[0-9])[:]([0-5][0-9])";
+            Regex re = new Regex(pattern, RegexOptions.Compiled);
+            Regex Dre = new Regex(datePattern, RegexOptions.Compiled);
+            Regex Tre = new Regex(timePattern, RegexOptions.Compiled);
+
+            //segmentlerin tanımlanması ve başlıklandırılması 
+            int c = 1;
+            using (var iter = output.GetIterator())
+            {
+                iter.Begin();
+                do
+                {
+                    if (!String.IsNullOrWhiteSpace(iter.GetText(PageIteratorLevel.Block)))
+                    {
+                        string segmentLabel = "";
+                        if (c == 1)
+                        {
+                            segmentLabel += "Market Bilgileri";
+                        }
+                        string segmentText = iter.GetText(PageIteratorLevel.Block).ToString();
+                        string[] segmentWords = segmentText.Split(' ');
+
+                        if (t && segmentText.Contains("%") && !segmentText.Contains("#"))
+                        {
+
+                            if (!segmentLabel.Contains("Urun Ve Fiyat Bilgileri"))
+                            {
+                                segmentLabel += "Urun Bilgileri" + Environment.NewLine;
+                            }
+
+                        }
+                        foreach (string word in segmentWords)
+                        {
+                            if (t && word.Any(char.IsDigit))
+                            {
+                                foreach (string s in zemberek.oner(word))
+                                {
+
+                                    if (s == "toplam" || s == "Toplam" || s == "TOPLAM")
+                                    {
+                                        segmentLabel += "Fiyat Ve Toplam Bilgileri" + Environment.NewLine;
+                                        t = false;
+
+                                    }
+                                    if (s == "tarih" || s == "Tarih" || s == "TARİH" || s == "TARIH" || s == "saat" || s == "Saat" || s == "SAAT")
+                                    {
+                                        Match m = Dre.Match(segmentText);
+                                        if (m.Success)
+                                        {
+                                            if (!segmentLabel.Contains("Tarih ve Saat Bilgileri"))
+                                            {
+                                                segmentLabel += "Tarih ve Saat Bilgileri" + Environment.NewLine;
+                                            }
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+
+                        //segment konum bilgilerinin kullanıcıya gösteriminin yapılması
+                        var blockType = iter.BlockType;
+                        Rect bounds = Rect.Empty;
+                        iter.TryGetBoundingBox(PageIteratorLevel.Block, out bounds);
+
+                        Pen pen = new Pen(Color.Red, 2);
+
+                        Graphics g = Graphics.FromImage(img);
+                        g.DrawRectangle(pen, bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        g.DrawString(segmentLabel, new Font("Tahoma", 16), Brushes.Red, bounds.X1 + bounds.Width - 180, bounds.Y1);
+
+                        c++;
+                    }
+
+                } while (iter.Next(PageIteratorLevel.Block));
+
+            }
+
+            //ocr dan elde edilen metin bilgisinde satır satır ilerleyip kural tabanını uygulama kısmı
+            t = true;
+            string linePrev = "";
+            for (int l = 0; l < line.Length; l++)
+            {
+                string[] lineWords = line[l].Split(' ');
+
+                for (int i = 0; i < lineWords.Length; i++) // Her bir sözcüğün teker teker kontrol edilmesi
+                {
+                    if (t && line[l].Any(char.IsDigit))
+                    {
+                        foreach (string s in zemberek.oner(lineWords[i])) //Nlp önerisi ve doğrulama
+                        {                           
+                            if (s == "toplam" || s == "Toplam" || s == "TOPLAM")
+                            {
+                                toplam = DigitOnly(line[l]);
+                                label5.Text = toplam.ToString();
+                                Console.WriteLine("Toplam: " + toplam);
+                                t = false;
+                                break;
+                            }
+                            if (s == "tarih" || s == "Tarih" || s == "TARİH" || s == "TARIH")
+                            {
+                                label1.Text = s;
+                                tarih = lineWords[i];
+                                label1.Text = tarih;
+                                Match m = Dre.Match(line[l]);
+                                if (m.Success)
+                                {                                                                      
+                                    tarih = m.Groups[2].ToString() + "/" + m.Groups[3].ToString() + "/" + m.Groups[4].ToString();
+                                    label1.Text = tarih;
+                                }
+                            }
+                            if (s == "saat" || s == "Saat" || s == "SAAT")
+                            {
+                                saat = lineWords[i];
+                                label2.Text = saat;
+                                Match m = Tre.Match(line[l]);
+                                if (m.Success)
+                                {                                                                     
+                                    saat = m.Groups[1].ToString() + ":" + m.Groups[2].ToString();
+                                    label2.Text = saat;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (t && line[l].Contains("%"))//satırın % KDV içeriğ içermediğinin kontrolü ve regexl işlemine alınması
+                {                    
+                    Match m = re.Match(line[l]);
+                    if (m.Success)
+                    {                        
+                        string miktar = "";
+                        Match a = re.Match(linePrev);
+                        if (!a.Success)
+                        {
+                            if (linePrev.Contains("AD") || linePrev.Contains("ADET") || linePrev.Contains("KG") || linePrev.Contains("GR"))
+                            {
+                                miktar = linePrev;
+                            }
+                        }                       
+                        float fiyat = DigitOnly(m.Groups[3].ToString());
+                        string[] row = { miktar + " " + m.Groups[1].ToString(), m.Groups[2].ToString(), fiyat.ToString() };
+                        var satir = new ListViewItem(row);
+                        listView1.Items.Add(satir);
+                        toplamKontrol += fiyat;                       
+                    }
+                }
+                if (!String.IsNullOrWhiteSpace(line[l]))
+                {
+                    linePrev = line[l];
+                }
+            }
+            if (toplam != toplamKontrol)//ürün fiyatlarının toplanıp toplam başlığı bilgisi ile karşılaştırılması
+            {
+                listView1.BackColor = Color.Orange;
+            }
+
+        }
+    }
+}
+
